ae1ad4b873fd27844fb903f0f9e7ccd3949b2e25
#云淡然 <cloudcome@163.com>
2018-08-09 11:45:27 +0800

    ~ 新建 style.css 文件，与原始无关

--- index.js
+++ index.js
@@ -8,6 +8,8 @@
 'use strict';
 
 var path = require('path');
+var fs = require('fs');
+var string = require('blear.utils.string');
 
 var pkg = require('./package.json');
 
@@ -23,12 +25,21 @@ exports.postConfig = function () {
             return options;
         }
 
+        var destDirname = options.configs.destDirname;
         var destName = path.basename(options.configs.dest.dirname);
         var destHost = options.configs.destHost;
 
         if (destHost === '/') {
             options.configs.destHost = '/wp-content/themes/' + destName + '/';
         }
+
+        var cssFile = path.join(destDirname, 'style.css');
+        var charset = 'utf-8';
+        var cssData = fs.readFileSync(path.join(__dirname, './style.css'), charset);
+        cssData = string.assign(cssData, {
+           themeName: destName
+        });
+        fs.writeFileSync(cssFile, cssData, charset);
 
         return options;
     };
@@ -51,7 +62,7 @@ exports.preStatic = function () {
         options.path = options.path.replace(/\/wp-content\/themes\/[^/]+\//, '/');
         return options;
     };
-    
+
     mid.package = pkg;
     return mid;
 };
