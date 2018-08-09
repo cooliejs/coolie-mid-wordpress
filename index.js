574f88393e156e9ac60713ef815fe8b7b98c6ae4
#云淡然 <cloudcome@163.com>
2018-08-09 15:52:49 +0800

    ~ 规范 coolie 中间件

--- index.js
+++ index.js
@@ -9,61 +9,76 @@
 
 var path = require('path');
 var fs = require('fs');
+var object = require('blear.utils.object');
 var string = require('blear.utils.string');
 
 var pkg = require('./package.json');
 
-var defaults = {};
+var defaults = {
+    /**
+     * 目标主题名称，为 `null` 表示根据目录名自动生成
+     * @type string | null
+     */
+    destThemeName: null
+};
 
-/**
- * 后置处理配置
- * @returns {Function}
- */
-module.exports.postConfig = function () {
+
+module.exports = function (configs) {
+    configs = object.assign({}, defaults, configs);
     return function (options) {
-        if (options.progress !== 'post-config') {
-            return options;
-        }
-
-        var destDirname = options.configs.destDirname;
-        var destName = path.basename(options.configs.dest.dirname);
-        var destHost = options.configs.destHost;
-
-        if (destHost === '/') {
-            options.configs.destHost = '/wp-content/themes/' + destName + '/';
-        }
-
-        var cssFile = path.join(destDirname, 'style.css');
-        var charset = 'utf-8';
-        var cssData = fs.readFileSync(path.join(__dirname, './style.css'), charset);
-        cssData = string.assign(cssData, {
-           themeName: destName
-        });
-        fs.writeFileSync(cssFile, cssData, charset);
-
-        return options;
+        return preStatic(
+            postConfig(options, configs),
+            configs
+        );
     };
 };
-
-
-/**
- * 前置处理静态资源
- * @returns {Function}
- */
-module.exports.preStatic = function () {
-    var mid = function (options) {
-        if (options.progress !== 'pre-static') {
-            return options;
-        }
-
-        options.path = options.path.replace(/\/wp-content\/themes\/[^/]+\//, '/');
-        return options;
-    };
-
-    mid.package = pkg;
-    return mid;
-};
-
-
 module.exports.package = pkg;
 module.exports.defaults = defaults;
+
+// ====================================
+/**
+ * 配置后置
+ * @param options
+ * @param configs
+ * @returns {*}
+ */
+function postConfig(options, configs) {
+    if (options.progress !== 'post-config') {
+        return options;
+    }
+
+    var destDirname = options.configs.destDirname;
+    var destName = path.basename(options.configs.dest.dirname);
+    var destHost = options.configs.destHost;
+    var themeName = configs.themeName || destName;
+
+    if (destHost === '/') {
+        options.configs.destHost = '/wp-content/themes/' + destName + '/';
+    }
+
+    var cssFile = path.join(destDirname, 'style.css');
+    var charset = 'utf-8';
+    var cssData = fs.readFileSync(path.join(__dirname, './style.css'), charset);
+    cssData = string.assign(cssData, {
+        themeName: themeName
+    });
+    fs.writeFileSync(cssFile, cssData, charset);
+
+    return options;
+}
+
+
+/**
+ * 静态前置
+ * @param options
+ * @param configs
+ * @returns {*}
+ */
+function preStatic(options, configs) {
+    if (options.progress !== 'pre-static') {
+        return options;
+    }
+
+    options.path = options.path.replace(/\/wp-content\/themes\/[^/]+\//, '/');
+    return options;
+}
